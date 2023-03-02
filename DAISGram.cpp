522825e8dc1078aef9bfd610d83250010baa6a7d
CippoX <tommypalmi01@gmail.com>
2023-03-02 15:49:28 +0100

    first commit

---
+++ DAISGram.cpp
@@ -0,0 +1,484 @@
+#include <iostream>
+#include <string>
+
+#include "dais_exc.h"
+#include "tensor.h"
+#include "libbmp.h"
+#include "DAISGram.h"
+
+using namespace std;
+
+DAISGram::DAISGram(){
+
+}
+
+DAISGram::~DAISGram(){
+    
+}
+
+/**
+ * Load a bitmap from file
+ *
+ * @param filename String containing the path of the file
+ */
+void DAISGram::load_image(string filename){
+    BmpImg img = BmpImg();
+
+    img.read(filename.c_str());
+
+    const int h = img.get_height();
+    const int w = img.get_width();
+
+    data = Tensor(h, w, 3, 0.0);
+
+    for(int i=0;i<img.get_height();i++){
+        for(int j=0;j<img.get_width();j++){
+            data(i,j,0) = (float) img.red_at(j,i);
+            data(i,j,1) = (float) img.green_at(j,i);
+            data(i,j,2) = (float) img.blue_at(j,i);
+        }
+    }
+}
+
+
+/**
+ * Save a DAISGram object to a bitmap file.
+ *
+ * Data is clamped to 0,255 before saving it.
+ *
+ * @param filename String containing the path where to store the image.
+ */
+void DAISGram::save_image(string filename){
+
+    data.clamp(0,255);
+
+    BmpImg img = BmpImg(getCols(), getRows());
+
+    img.init(getCols(), getRows());
+
+    for(int i=0;i<getRows();i++){
+        for(int j=0;j<getCols();j++){
+            img.set_pixel(j,i,(unsigned char) data(i,j,0),(unsigned char) data(i,j,1),(unsigned char) data(i,j,2));
+        }
+    }
+
+    img.write(filename);
+
+}
+
+/**
+ * Get rows
+ *
+ * @return returns the number of rows in the image
+ */
+int DAISGram::getRows(){
+    return data.rows();
+}
+
+/**
+ * Get columns
+ *
+ * @return returns the number of columns in the image
+ */
+int DAISGram::getCols(){
+    return data.cols();
+}
+
+/**
+ * Get depth
+ *
+ * @return returns the number of channels in the image
+ */
+int DAISGram::getDepth(){
+    return data.depth();
+}
+
+/**
+ * Brighten the image
+ *
+ * It sums the bright variable to all the values in the image.
+ *
+ * Before returning the image, the corresponding tensor should be clamped in [0,255]
+ *
+ * @param bright the amount of bright to add (if negative the image gets darker)
+ * @return returns a new DAISGram containing the modified object
+ */
+DAISGram DAISGram::brighten(float bright){
+    DAISGram res{};
+    res.data = data + bright;
+    res.data.clamp(0, 255);
+    return res;
+}
+
+/**
+ * Create a grayscale version of the object
+ *
+ * A grayscale image is produced by substituting each pixel with its average on all the channel
+ *
+ * @return returns a new DAISGram containing the modified object
+ */
+DAISGram DAISGram::grayscale(){
+    DAISGram res = *this;
+    int average = 0;
+    for(int i = 0; i < data.rows(); i++){
+        for(int j = 0; j < data.cols(); j++){
+            for(int k = 0; k < data.depth(); k++){
+                average += res.data(i, j, k);
+            }
+            for(int k = 0; k < data.depth(); k++){
+                res.data(i, j, k) = average / data.depth();
+            }
+            average = 0;
+        }
+    }
+        
+    return res;
+}
+
+/**
+ * Create a Warhol effect on the image
+ *
+ * This function returns a composition of 4 different images in which the:
+ * - top left is the original image
+ * - top right is the original image in which the Red and Green channel are swapped
+ * - bottom left is the original image in which the Blue and Green channel are swapped
+ * - bottom right is the original image in which the Red and Blue channel are swapped
+ *
+ * The output image is twice the dimensions of the original one.
+ *
+ * @return returns a new DAISGram containing the modified object
+ */
+DAISGram DAISGram::warhol(){
+    DAISGram original = *this;
+    DAISGram green = *this;
+    DAISGram red = *this;
+    DAISGram blue = *this;
+
+    //GREEN <-> RED
+    for(int i = 0; i < data.rows(); i++){
+        for(int j = 0; j < data.cols(); j++){
+            float supp;
+
+            //GREEN <-> RED
+            supp = green.data(i, j, 0);
+            green.data(i, j, 0) = green.data(i, j, 1);
+            green.data(i, j, 1) = supp;
+
+            //GREEN <-> BLUE
+            supp = red.data(i, j, 1);
+            red.data(i, j, 1) = red.data(i, j, 2);
+            red.data(i, j, 2) = supp;
+
+            //RED <-> BLUE
+            supp = blue.data(i, j, 0);
+            blue.data(i, j, 0) = blue.data(i, j, 2);
+            blue.data(i, j, 2) = supp;
+        }
+    }
+
+    DAISGram original_green, red_blue, res;
+    original_green.data = original.data.concat(green.data, 1);
+    red_blue.data = red.data.concat(blue.data, 1);
+    res.data =  original_green.data.concat(red_blue.data, 0);
+
+    return res;
+}
+
+/**
+ * Sharpen the image
+ *
+ * This function makes the image sharper by convolving it with a sharp filter
+ *
+ * filter[3][3]
+ *    0  -1  0
+ *    -1  5 -1
+ *    0  -1  0
+ *
+ * Before returning the image, the corresponding tensor should be clamped in [0,255]
+ *
+ * @return returns a new DAISGram containing the modified object
+ */
+DAISGram DAISGram::sharpen(){
+    Tensor filter{3, 3, 3};
+    
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            for(int k = 0; k < 3; k++){
+                if(i == 1 && j == 1){
+                    filter(i, j, k) = 5;
+                }else if(i % 2 != 0 || j % 2 != 0){
+                    filter(i, j, k) = -1;
+                }else{
+                    filter(i, j, k) = 0;
+                }
+            }
+        }
+    }
+
+    DAISGram res{};
+    res.data = data.convolve(filter);
+    res.data.clamp(0, 255);
+    return res;
+}
+
+/**
+ * Emboss the image
+ *
+ * This function makes the image embossed (a light 3D effect) by convolving it with an
+ * embossing filter
+ *
+ * filter[3][3]
+ *    -2 -1  0
+ *    -1  1  1
+ *     0  1  2
+ *
+ * Before returning the image, the corresponding tensor should be clamped in [0,255]
+ *
+ * @return returns a new DAISGram containing the modified object
+ */
+DAISGram DAISGram::emboss(){
+    Tensor filter{3, 3, 3};
+
+    int z = -3;
+    for(int i = 0; i < 3; i++){
+        z++;
+        int temp = z-1;
+        for(int j = 0; j < 3; j++){
+            temp++;
+            for(int k = 0; k < 3; k++){
+                if(i == 1 && j == 1){
+                    filter(i, j, k) = 1;
+                }else{
+                    filter(i, j, k) = temp;
+                }
+            }
+        }
+    }
+
+    DAISGram temp = *this;
+    DAISGram res;
+    res.data = temp.data.convolve(filter);
+    res.data.clamp(0, 255);
+    return res;
+}
+
+/**
+ * Smooth the image
+ *
+ * This function remove the noise in an image using convolution and an average filter
+ * of size h*h:
+ *
+ * c = 1/(h*h)
+ *
+ * filter[3][3]
+ *    c c c
+ *    c c c
+ *    c c c
+ *
+ * @param h the size of the filter
+ * @return returns a new DAISGram containing the modified object
+ */
+DAISGram DAISGram::smooth(int h){
+    float c = 1.0f / (h * h);
+    Tensor filter{h, h, 3, c};
+
+    DAISGram temp = *this;
+    DAISGram res;
+    res.data = temp.data.convolve(filter);
+    return res;
+}
+
+/**
+ * Edges of an image
+ *
+ * This function extract the edges of an image by using the convolution
+ * operator and the following filter
+ *
+ *
+ * filter[3][3]
+ * -1  -1  -1
+ * -1   8  -1
+ * -1  -1  -1
+ *
+ * Remeber to convert the image to grayscale before running the convolution.
+ *
+ * Before returning the image, the corresponding tensor should be clamped in [0,255]
+ *
+ * @return returns a new DAISGram containing the modified object
+ */
+DAISGram DAISGram::edge(){
+    Tensor filter{3, 3, 3};
+    
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            for(int k = 0; k < 3; k++){
+                if(i == 1 && j == 1){
+                    filter(i, j, k) = 8;
+                }else{
+                    filter(i, j, k) = -1;
+                }
+            }
+        }
+    }
+    
+    DAISGram temp = *this;
+    temp = temp.grayscale();
+    DAISGram res;
+    res.data = temp.data.convolve(filter);
+    return res;
+}
+
+/**
+ * Blend with anoter image
+ *
+ * This function generate a new DAISGram which is the composition
+ * of the object and another DAISGram object
+ *
+ * The composition follows this convex combination:
+ * results = alpha*this + (1-alpha)*rhs
+ *
+ * rhs and this obejct MUST have the same dimensions.
+ *
+ * @param rhs The second image involved in the blending
+ * @param alpha The parameter of the convex combination
+ * @return returns a new DAISGram containing the blending of the two images.
+ */
+DAISGram DAISGram::blend(const DAISGram & rhs, float alpha){
+    DAISGram check = rhs;
+    if(getRows() != check.getRows() || getCols() != check.getCols() || getDepth() != check.getDepth()) throw(dimension_mismatch());
+    DAISGram res;
+    res.data = data * alpha + check.data * (1 - alpha);
+    return res;
+}
+
+
+/**
+ * Generate Random Image
+ *
+ * Generate a random image from nois
+ *
+ * @param h height of the image
+ * @param w width of the image
+ * @param d number of channels
+ * @return returns a new DAISGram containing the generated image.
+ */
+void DAISGram::generate_random(int h, int w, int d){
+    data = Tensor(h,w,d,0.0);
+    data.init_random(128,50);
+    data.rescale(255);
+}
+
+//PARTI OPZIONALI
+
+/**
+ * Green Screen
+ *
+ * This function substitutes a pixel with the corresponding one in a background image
+ * if its colors are in the surrounding (+- threshold) of a given color (rgb).
+ *
+ * (rgb - threshold) <= pixel <= (rgb + threshold)
+ *
+ *
+ * @param bkg The second image used as background
+ * @param rgb[] The color to substitute (rgb[0] = RED, rgb[1]=GREEN, rgb[2]=BLUE)
+ * @param threshold[] The threshold to add/remove for each color (threshold[0] = RED, threshold[1]=GREEN, threshold[2]=BLUE)
+ * @return returns a new DAISGram containing the result.
+ */
+DAISGram DAISGram::greenscreen(DAISGram & bkg, int rgb[], float threshold[]){
+    if(data.rows() != bkg.data.rows() || data.cols() != bkg.data.cols() ||data.depth() != bkg.data.depth()) throw(dimension_mismatch());
+    DAISGram res = *this;
+    int counter = 0;
+    
+    for(int i = 0; i < res.getRows(); i++){
+        for(int j = 0; j < res.getCols(); j++){
+            for(int k = 0; k < res.getDepth(); k++){
+                if(res.data(i, j, k) >= (rgb[k] - threshold[k]) && res.data(i, j, k) <= (rgb[k] + threshold[k])){
+                    counter++;
+                }
+            }
+
+            if(counter == 3){
+                res.data(i, j, 0) = bkg.data(i, j, 0);
+                res.data(i, j, 1) = bkg.data(i, j, 1);
+                res.data(i, j, 2) = bkg.data(i, j, 2);
+            }
+            counter = 0;
+        }
+    }
+
+    return res;
+}
+
+/**
+ * Equalize
+ *
+ * Stretch the distribution of colors of the image in order to use the full range of intesities.
+ *
+ * See https://it.wikipedia.org/wiki/Equalizzazione_dell%27istogramma
+ *
+ * @return returns a new DAISGram containing the equalized image.
+ */
+DAISGram DAISGram::equalize(){
+    DAISGram res = *this;
+    Tensor occurrencies{16, 16, 3};
+    int rows = res.getRows();
+    int cols = res.getCols();
+
+    int a = 0, b = 0;
+
+    for(int z = res.data.getMin(0); z < 256; z++){
+        occurrencies(a, b, 0) = z;
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(res.data(i, j, 0) == z){
+                    occurrencies(a, b, 1)++;
+                }
+            }
+        }
+        b++;
+        if(b == 16){
+            a++;
+            b = 0;
+        }
+    }
+
+    rows = occurrencies.rows();
+    cols = occurrencies.cols();
+
+    float cdf = 0.0f;
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+            if(occurrencies(i, j, 1) != 0){
+                cdf += occurrencies(i, j, 1);
+                occurrencies(i, j, 2) = cdf;
+            }
+        }
+    }
+
+    float cdf_min = occurrencies(0, 0, 2);
+    int dimension = res.getRows() * res.getCols();
+    int L = 256;
+
+    rows = res.getRows();
+    cols = res.getCols();
+    int depth = res.getDepth();
+
+    a = 0;
+    b = 0;
+
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+            for(int k = 0; k < depth; k++){
+                if(res.data(i, j, k) != 0){
+                    cdf = occurrencies.getCDF(res.data(i, j, k));
+
+                    if(cdf != 0){
+                        res.data(i, j, k) = round( ((cdf - cdf_min) / (dimension - cdf_min)) * (L - 1) );
+                    }
+                }
+            }
+        }
+    }
+
+    return res;
+}
---
+++ tensor.cpp
@@ -0,0 +1,463 @@
+#include <iostream>
+#include <string>
+#include <random>
+#include <math.h>
+#include <fstream>
+
+#include "dais_exc.h"
+#include "tensor.h"
+
+#define PI 3.141592654
+#define FLT_MAX 3.402823466e+38F /* max value */
+#define FLT_MIN 1.175494351e-38F /* min positive value */
+
+using namespace std;
+
+Tensor::Tensor(){
+    data = nullptr;
+    r = 0;
+    c = 0;
+    d = 0;
+} 
+
+void Tensor::allocate_memory(){
+    data = new float**[r];
+
+    for(int i = 0; i < r; i++){
+        data[i] = new float*[c];
+        for(int j = 0; j < c; j++){
+            data[i][j] = new float[d];
+        }
+    }
+}
+
+Tensor::Tensor(int r, int c, int d, float v) {
+    this->r = r;
+    this->c = c;
+    this->d = d;
+
+    allocate_memory();
+
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < c; j++)
+            for(int k = 0; k < d; k++)
+                data[i][j][k] = v;
+}
+
+Tensor::~Tensor(){
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++)
+            delete[] data[i][j];
+        delete[] data[i];
+    }
+
+    delete[] data;
+}
+
+float Tensor::operator()(int i, int j, int k) const{
+    if((i < 0 || i >= r) || (j < 0 || j >= c) || (k < 0 || k >= d)) throw(index_out_of_bound());
+    else return data[i][j][k];
+}
+
+float& Tensor::operator()(int i, int j, int k){
+    if((i < 0 || i >= r) || (j < 0 || j >= c) || (k < 0 || k >= d)) throw(index_out_of_bound());
+    else return data[i][j][k];
+}
+
+Tensor::Tensor(const Tensor& that){
+    this->r = that.r;
+    this->c = that.c;
+    this->d = that.d;
+
+    allocate_memory();
+
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < c; j++)
+            for(int k = 0; k < d; k++)
+                data[i][j][k] = that(i, j, k);
+}
+
+bool Tensor::operator==(const Tensor& rhs) const{
+    if(r != rhs.r || c != rhs.c || d != rhs.d) throw(dimension_mismatch());
+
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            for(int k = 0; k < d; k++){
+                if(abs(data[i][j][k] - rhs(i, j, k)) > EPSILON) return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+Tensor Tensor::operator-(const Tensor &rhs){
+    if(r != rhs.r || c != rhs.c || d != rhs.d) throw(dimension_mismatch());
+    Tensor res{r, c, d};
+
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < c; j++)
+            for(int k = 0; k < d; k++)
+                res(i, j, k) = data[i][j][k] - rhs(i, j, k);
+    
+    return res;
+}
+
+Tensor Tensor::operator+(const Tensor &rhs){
+    if(r != rhs.r || c != rhs.c || d != rhs.d) throw(dimension_mismatch());
+    Tensor res{r, c, d};
+
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < c; j++)
+            for(int k = 0; k < d; k++)
+                res(i, j, k) = data[i][j][k] + rhs(i, j, k);
+    
+    return res;
+}
+
+Tensor Tensor::operator*(const Tensor &rhs){
+    if(r != rhs.r || c != rhs.c || d != rhs.d) throw(dimension_mismatch());
+    Tensor res{r, c, d};
+
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < c; j++)
+            for(int k = 0; k < d; k++)
+                res(i, j, k) = data[i][j][k] * rhs(i, j, k);
+    
+    return res;
+}
+
+Tensor Tensor::operator/(const Tensor &rhs){
+    if(r != rhs.r || c != rhs.c || d != rhs.d) throw(dimension_mismatch());
+    Tensor res{r, c, d};
+
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < c; j++)
+            for(int k = 0; k < d; k++)
+                res(i, j, k) = data[i][j][k] / rhs(i, j, k);
+    
+    return res;
+}
+
+Tensor Tensor::operator-(const float &rhs){
+    Tensor res{r, c, d};
+
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < c; j++)
+            for(int k = 0; k < d; k++)
+                res(i, j, k) = data[i][j][k] - rhs;
+    
+    return res;
+}
+
+Tensor Tensor::operator+(const float &rhs){
+    Tensor res{r, c, d};
+
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < c; j++)
+            for(int k = 0; k < d; k++)
+                res(i, j, k) = data[i][j][k] + rhs;
+    
+    return res;
+}
+
+Tensor Tensor::operator*(const float &rhs){
+    Tensor res{r, c, d};
+
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < c; j++)
+            for(int k = 0; k < d; k++)
+                res(i, j, k) = data[i][j][k] * rhs;
+    
+    return res;
+}
+
+Tensor Tensor::operator/(const float &rhs){
+    Tensor res{r, c, d};
+
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < c; j++)
+            for(int k = 0; k < d; k++)
+                res(i, j, k) = data[i][j][k] / rhs;
+    
+    return res;
+}
+
+Tensor & Tensor::operator=(const Tensor &other){
+    this->r = other.r;
+    this->c = other.c;
+    this->d = other.d;
+
+    allocate_memory();
+
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < c; j++)
+            for(int k = 0; k < d; k++)
+                data[i][j][k] = other(i, j, k);
+    
+    return *this;
+}
+
+void Tensor::init_random(float mean, float std){
+    if(data){
+
+        std::default_random_engine generator;
+        std::normal_distribution<float> distribution(mean,std);
+
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                for(int k=0;k<d;k++){
+                    this->operator()(i,j,k)= distribution(generator);
+                }
+            }
+        }
+
+    }else{
+        throw(tensor_not_initialized());
+    }
+}
+
+void Tensor::init(int r, int c, int d, float v){
+    if(data){
+        for(int i = 0; i < r; i++)
+            for(int j = 0; j < c; j++)
+                for(int k = 0; k < d; k++)
+                    data[i][j][k] = v;
+    }else{
+        throw(tensor_not_initialized());
+    }
+}
+
+void Tensor::clamp(float low, float high){
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            for(int k = 0; k < d; k++){
+                if(data[i][j][k] < low) data[i][j][k] = low;
+                if(data[i][j][k] > high) data[i][j][k] = high;
+            }
+        }
+    }
+}
+
+void Tensor::rescale(float new_max){
+    for (int k = 0; k < d; k++) {
+        int Min = getMin(k), Max = getMax(k);
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                data[i][j][k] = ((data[i][j][k] - Min) / (Max - Min)) * new_max;
+            }
+        }
+    }
+}
+
+Tensor Tensor::padding(int pad_h, int pad_w){
+    Tensor res{r + pad_h * 2, c + pad_w * 2, d};
+    
+    for(int i = pad_h; i < res.r - pad_h; i++){
+        for(int j = pad_w; j < res.c - pad_w; j++){
+            for(int k = 0; k < res.d; k++){
+                res(i, j, k) = data[i - pad_h][j - pad_w][k];
+            }
+        }
+    }
+        
+    return res;
+}
+
+Tensor Tensor::subset(unsigned int row_start, unsigned int row_end, unsigned int col_start, unsigned int col_end, unsigned int depth_start, unsigned int depth_end){
+    int new_row = (int) row_end - row_start;
+    int new_col = (int) col_end - col_start;
+    int new_depth = (int) depth_end - depth_start;
+    
+    Tensor res{new_row, new_col, new_depth};
+
+    for(int i = row_start; i < row_end; i++)
+        for(int j = col_start; j < col_end; j++)
+            for(int k = depth_start; k < depth_end; k++)
+                res(i - row_start, j - col_start, k - depth_start) = data[i][j][k];
+            
+    return res;
+}
+
+Tensor Tensor::concat(const Tensor &rhs, int axis){
+    Tensor res{};
+    if(axis == 0){
+        if(c != rhs.c || d != rhs.d) throw(concat_wrong_dimension());
+        Tensor temp{r + rhs.r, c, d};
+
+        for(int i = 0; i < temp.rows(); i++){
+            for(int j = 0; j < temp.cols(); j++){
+                for(int k = 0; k < temp.depth(); k++){
+                    if(i < rows()){
+                        temp(i, j, k) = data[i][j][k];
+                    }else{
+                        temp(i, j, k) = rhs(i - rows(), j, k);
+                    }
+                }
+            }
+        }
+
+        res = temp;
+    }else if(axis == 1){
+        if(r != rhs.r || d != rhs.d) throw(concat_wrong_dimension());
+        Tensor temp{r , c + rhs.c, d};
+
+        for(int i = 0; i < temp.rows(); i++){
+            for(int j = 0; j < temp.cols(); j++){
+                for(int k = 0; k < temp.depth(); k++){
+                    if(j < cols()){
+                        temp(i, j, k) = data[i][j][k];
+                    }else{
+                        temp(i, j, k) = rhs(i, j - cols(), k);
+                    }
+                }
+            }
+        }
+
+        res = temp;
+    }else if(axis == 2){
+        if(r != rhs.r || c != rhs.c) throw(concat_wrong_dimension());
+        Tensor temp{r , c, d + rhs.d};
+
+        for(int i = 0; i < temp.rows(); i++){
+            for(int j = 0; j < temp.cols(); j++){
+                for(int k = 0; k < temp.depth(); k++){
+                    if(k < depth()){
+                        temp(i, j, k) = data[i][j][k];
+                    }else{
+                        temp(i, j, k) = rhs(i, j, k - depth());
+                    }
+                }
+            }
+        }
+
+        res = temp;
+    }else{
+        throw(concat_wrong_dimension());
+    }
+
+    return res;
+}
+
+Tensor Tensor::convolve(const Tensor &f){
+    Tensor f1 = f;
+    if((f1.rows() != f1.cols()) || (f1.rows() % 2 == 0) || (f1.cols() % 2 == 0) || f1.depth() != depth()) throw(dimension_mismatch());
+    Tensor res = *this;
+    
+    int pad_h = (f1.rows() - 1) / 2;
+    int pad_w = (f1.cols() - 1) / 2;
+
+    Tensor temp = res.padding(pad_h, pad_w);;
+
+    for(int k = 0; k < temp.depth(); k++){
+        
+        for(int i = 0; i <= temp.rows() - f1.rows(); i++){
+            for(int j = 0; j <= temp.cols() - f1.cols(); j++){
+                Tensor supp = temp.subset(i, i + f1.rows(), j, j + f1.cols(), k, k + 1);
+                float sum = 0;
+                for(int a = 0; a < supp.rows(); a++){
+                    for(int b = 0; b < supp.cols(); b++){
+                        sum += (supp(a, b, 0) * f1(a, b, k));
+                    }
+                }
+
+                res(i, j, k) = sum;
+            }
+        }
+    }
+
+    return res;
+}
+
+int Tensor::rows(){
+    return r;
+}
+
+int Tensor::cols(){
+    return c;
+}
+
+int Tensor::depth(){
+    return d;
+}
+
+float Tensor::getMin(int k){
+    float res = data[0][0][k];
+
+    for(int i = 0; i < r; i++){
+        for(int j = 1; j < c; j++){
+            if(data[i][j][k] < res) res = data[i][j][k];
+        }
+    }
+
+    return res;
+}
+
+float Tensor::getMax(int k){
+    float res = data[0][0][k];
+
+    for(int i = 0; i < r; i++){
+        for(int j = 1; j < c; j++){
+            if(data[i][j][k] > res) res = data[i][j][k];
+        }
+    }
+
+    return res;
+}
+
+void Tensor::showSize(){
+    cout << r << " x " << c << " x " << d << endl;
+}
+
+ostream& operator<<(ostream& stream, const Tensor & obj){
+    for(int k = 0; k < obj.d; k++){
+        for(int i = 0; i < obj.r; i++){
+            stream << "[ ";
+            for(int j = 0; j < obj.c; j++){
+                stream << obj.data[i][j][k] << " ";
+            }
+            stream << "]\n";
+        }
+        stream << "\n";
+    }
+    return stream;
+}
+
+void Tensor::read_file(string filename){
+    ifstream f{filename};
+    if(!f) throw(unable_to_read_file());
+
+    string line;
+    f >> r >> c >> d;
+    
+    allocate_memory();
+
+    for(int k = 0; k < d; k++)
+        for(int i = 0; i < r; i++)
+            for(int j = 0; j < c; j++)
+                f >> data[i][j][k];
+
+    f.close();
+}
+
+void Tensor::write_file(string filename){
+    ofstream f{filename};
+    f << r << "\n" << c << "\n" << d << "\n";
+
+    for(int k = 0; k < d; k++)
+        for(int i = 0; i < r; i++)
+            for(int j = 0; j < c; j++)
+                f << data[i][j][k] << "\n";
+    
+    f.close();
+}
+
+int Tensor::getCDF(int value){
+    for(int i = 0; i < rows(); i++){
+        for(int j = 0; j < cols(); j++){
+            if(data[i][j][0] == value) return data[i][j][2];
+        }
+    }
+
+    return 0;
+}
